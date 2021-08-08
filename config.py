2b880448ae1dcae18a707c18cdadcefc8510b17c
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-08 17:26:51 +0300

    general configurations.

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    BASE_URL = 'https://newsapi.org/v2/top-headlines/sources?apiKey=86a8bd3b9cc84d4d915c2abba452c4f4'
+    NEWS_API_KEY = '86a8bd3b9cc84d4d915c2abba452c4f4'
+    NEWS_SOURCE_BASE_URL = 'https://newsapi.org/v2/sources?apiKey=86a8bd3b9cc84d4d915c2abba452c4f4'
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+} 
