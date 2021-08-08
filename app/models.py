8ba8f88b5a42bf55b373859a5380a0ab8c3def70
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-08 17:36:39 +0300

    created article class.

---
+++ app/models.py
@@ -0,0 +1,14 @@
+class Article:
+    '''
+    Class for news objects
+    '''
+    def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+        self.content = content
+        
+        
